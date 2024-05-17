e8ec52c05ca04a83f6846bee792a3db31aae191f
Rain Hou <165733077+lightrainqaq@users.noreply.github.com>
2024-05-17 15:45:07 -0700

    Update Third Chart.R

--- Third Chart.R
+++ Third Chart.R
@@ -1,9 +1,19 @@
-# Load ggplot2 library
+my_data <- data.frame(
+  status = c("adoptable", "not adoptable", "adoptable", "not adoptable", "adoptable"),
+  shots_current = c("yes", "no", "yes", "yes", "no")
+)
+
 library(ggplot2)
 
-contingency_table <- table(data$status, data$shots_current)
-mosaicplot(contingency_table, main="Mosaic Plot of status Category by shots_current", xlab="status", ylab="shots_current", color=TRUE)
 
+contingency_table <- table(my_data$status, my_data$shots_current)
+
+
+mosaicplot(contingency_table, 
+           main="Mosaic Plot of Status Category by Shots Current", 
+           xlab="Status", 
+           ylab="Shots Current", 
+           color=TRUE)
 #Analyzing the factors of "status" (adoptability) and "shots_current" 
 #(vaccination status) in relation to each other can provide valuable insights 
 #for various stakeholders, including animal shelters, rescue organizations, potential adopters, and public health authorities.
